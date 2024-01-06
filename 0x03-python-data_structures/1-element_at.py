59201fc2a93e805311255ec5528a5a3fd0d20180
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-06 12:29:49 -0800

    checker is messing with me

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,11 +3,13 @@
 
 
 def element_at(my_list, idx):
-    elt = my_list[idx]
+    element = my_list[idx]
+    n = len(my_list)
+
     for idx in my_list:
         if idx < 0:
             return None
-        if idx > len(my_list):
+        if idx > n:
             return None
         else:
-            return elt
+            return element
