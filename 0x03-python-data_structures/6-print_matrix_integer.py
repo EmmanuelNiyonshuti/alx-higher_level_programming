2456d4e55749bebe04e5f79d17e2b22f4cfa7ee5
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-07 19:20:41 -0800

    pycodestyle debug

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
     num_columns = len(matrix[0]) if matrix and matrix[0] else 0
     for i in range(num_rows):
         for j in range(num_columns):
-            print("{:d}".format(matrix[i][j]), end=" " if j < num_columns - 1 else "")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
