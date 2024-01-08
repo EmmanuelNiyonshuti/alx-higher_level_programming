312c8386d375054dda04047ed5cd0d9454eea813
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-08 09:38:24 -0800

    Deletes an item in a list at a specific index using del

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""deletes the item at a specific position in a list"""
+
+
+def delete_at(my_list=[], idx=0):
+
+    n = len(my_list)
+    if idx < 0 or idx > n:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
