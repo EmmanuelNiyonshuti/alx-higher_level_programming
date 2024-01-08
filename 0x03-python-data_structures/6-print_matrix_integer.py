bb27b15377f2dff1f67e666a702dbe1cd7efe456
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-07 18:52:19 -0800

    printing a matrix using nested for loop

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" prints a matrix of integers"""
+
+
+def print_matrix_integer(matrix=[[]]):
+    num_rows = len(matrix)
+    num_columns = len(matrix[0] if matrix else 0)
+    for i in range(num_rows):
+        for j in range(num_columns):
+            print(matrix[i][j], end="")
+        print()
