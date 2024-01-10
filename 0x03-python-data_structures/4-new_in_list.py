5af3cd7957ebc98902459e5be0757d29a2fec3e4
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-09 21:54:02 -0800

    Replacing an element in a list without modifying the original string

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,10 +3,12 @@
 
 
 def new_in_list(my_list, idx, element):
-    list = my_list[:]
-    for i in my_list:
-        if idx < 0 and idx > len(my_list):
-            return list
-        else:
-            list[idx] = element
-            return list
+
+    list_copy = my_list[:]
+    if idx < 0:
+        return list_copy
+    elif idx > len(my_list):
+        return list_copy
+    else:
+        list_copy[idx] = element
+        return list_copy
