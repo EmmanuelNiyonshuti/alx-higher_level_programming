42a8828469dee0cd51d659727258ead947513a29
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-09 22:15:29 -0800

    7 and 10 checker errors resolved

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,12 +4,8 @@
 
 def replace_in_list(my_list, idx, element):
 
-    n = len(my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
-    if idx < 0:
-        return my_list
-    elif idx > n:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    my_list[idx] = element
+    return my_list
