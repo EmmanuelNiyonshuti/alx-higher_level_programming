ef66b016dfc57a695a32f39f6396a631301c7335
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-11 08:46:18 -0800

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Prints a dictionary by ordered keys."""
+
+
+def print_sorted_dictionary(a_dictionary):
+
+    for key, value in sorted(a_dictionary.items()):
+        print("{} : {}".format(key, value))
