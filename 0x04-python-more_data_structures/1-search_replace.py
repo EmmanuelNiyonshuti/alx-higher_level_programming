ad27a2c9a02c3d87f61d0ead007cb331a4e5b42c
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-11 04:37:38 -0800

    replacing a value in a list using list comprehension

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""replaces all occurrences of an element by another in a new list."""
+
+
+def search_replace(my_list, search, replace):
+
+    new_list = [replace if x == search else x for x in my_list]
+
+    return new_list
