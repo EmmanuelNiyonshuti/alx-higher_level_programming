4ad3696531583d22a14f3b7e4f8bf5d7f61641dc
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-12 12:23:53 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,7 @@
 
 def element_at(my_list, idx):
 
-    if idx < 0:
-        return
-    elif idx > len(my_list):
-        return
-    else:
-        return my_list[idx]
+    if idx < 0 or idx > len(my_list):
+        return None
+
+    return my_list[idx]
