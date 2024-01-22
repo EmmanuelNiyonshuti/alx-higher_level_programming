6b9e5f0ddcd3daa0d1074311c0fbb42d26c331ef
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-22 06:37:56 -0800

    actual number of elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,11 @@ def safe_print_list(my_list=[], x=0):
 
     i = 0
     try:
-        for i in range(0, x):
+        for i in range(x):
             print(my_list[i], end='')
     except IndexError:
         pass
+
     print()
-    return i + 1
+
+    return i
