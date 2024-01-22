f6d77903a68b241464006014656243b1c0fa837e
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-22 06:33:39 -0800

    All elements must be printed on the same line followed by a new line.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for i in range(0, x):
-            print(my_list[i], end'')
+            print(my_list[i], end='')
     except IndexError:
         pass
+    print()
     return i
