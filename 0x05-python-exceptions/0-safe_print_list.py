63508a09df24c6e3e0beffbcde1a755cad90fa4e
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-22 06:28:46 -0800

    Prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""safe_print_list - prints x elements of a list."""
+
+
+def safe_print_list(my_list=[], x=0):
+
+    i = 0
+    try:
+        for i in range(0, x):
+            print(my_list[i], end'')
+    except IndexError:
+        pass
+    return i
