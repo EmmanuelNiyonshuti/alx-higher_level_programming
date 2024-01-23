b11bc658836773e950ea1aed6eec80e8d39d07a8
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-23 13:00:20 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,15 +3,11 @@
 
 
 def safe_print_division(a, b):
-    result = 0
 
     try:
-        result = a / b
-
+        return a / b
     except ZeroDivisionError:
         return None
 
     finally:
         print("Inside result:{}".format(result))
-
-    return result
