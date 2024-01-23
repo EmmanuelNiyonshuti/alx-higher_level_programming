aa913dcbf102acbb7f4a14327d655ed411e865a5
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-23 12:11:10 -0800

    prints an integer with {:d}.format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,8 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
-    except:
-        return False
-    else:
         return True
+    except TypeError:
+        pass
+    else:
+        return False
