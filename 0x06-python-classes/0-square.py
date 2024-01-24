aa3f3c8cf1342e0b2d6889dacc7909f9b7f6c701
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-24 13:40:40 -0800

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""Square empty class that defines a square"""
+"""Square Module: Contains the Square class"""
 
 
 class Square:
+    """Empty class Square that defines a square."""
     pass
