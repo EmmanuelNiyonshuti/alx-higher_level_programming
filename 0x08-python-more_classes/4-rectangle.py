69421b65aafa44eceb7471105e86bf0b0dc40350
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-31 14:34:51 -0800

    continues adding features to rectangle class repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval() (see example below

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""This module comprises a class of rectangle"""
+
+
+class Rectangle:
+    """Defines a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Instantiation with width and height"""
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __str__(self):
+        if self.__height == 0 or self.__height == 0:
+            return ""
+
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += "#" * self.__width + "\n"
+
+        return rectangle_str[:-1]
+    def __repr__(self):
+        """Return a string representation of eval()"""
+        return f"Rectangle({self.__width}, {self.__height})"
