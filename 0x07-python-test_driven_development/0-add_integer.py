0c94349022ce0c6e106275d5cd321e1927aec79a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-01 11:56:12 -0800

    handled when a nad b must be casted into ints if they are floats

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,7 +24,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+    if  isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
 
     return a + b
