1794ec79a8e2bf4e45f8eafcb517f818f3626c5c
Niyonshuti <nemmy0257@gmail.com>
2024-02-09 17:32:39 -0800

    pythonic way for adding unique int from a list in 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,12 +4,7 @@
 
 def uniq_add(my_list=[]):
 
-    unique_set = set()
-    total = 0
-
-    for num in my_list:
-        if num not in unique_set:
-            total += num
-            unique_set.add(num)
+    unique_set = set(my_list)
+    total = sum(unique_set)
 
     return total
