5f31ea4315a7bf434cdd78cc7902acae51dcd1df
Niyonshuti <nemmy0257@gmail.com>
2024-02-09 17:43:47 -0800

    Pythonic for returning a set of common elements in two sets.

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -4,9 +4,4 @@
 
 def common_elements(set_1, set_2):
 
-    common_element = set()
-
-    for element in set_1:
-        if element in set_2:
-            common_element.add(element)
-    return common_element
+    return set_1.intersection(set_2)
