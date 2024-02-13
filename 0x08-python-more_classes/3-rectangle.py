3348e74e56c801a398ee76ff90a830392ac3f8b1
Niyonshuti <nemmy0257@gmail.com>
2024-02-12 18:19:54 -0800

    stripped a new line

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
         Rectangle_str = ""
         for _ in range(self.__height):
             Rectangle_str += '#' * self.__width + "\n"
-        return Rectangle_str
+        return Rectangle_str.strip()
 
     def __repr__(self):
         """Print a rectangle with the character #"""
