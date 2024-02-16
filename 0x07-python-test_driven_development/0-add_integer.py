5728c204ed9ce997fddb0806657b8203fbf0aae6
Niyonshuti <nemmy0257@gmail.com>
2024-02-16 04:26:40 -0800

    Implemented function that adds two integers tested with almost all edge cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module comprises a function that adds integers.
+"""
+def add_integer(a, b=98):
+
+    """Adds two integers
+    Args:
+    a: Integer or float
+    b: Integer or float (default 98)
+
+    raises:
+    when a and b are not integers
+    TypeError with a message a must be an integer or b must be an integer.
+
+    return:
+    sum of a and b
+
+    return a + b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return a + b
