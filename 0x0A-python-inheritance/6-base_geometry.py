e5a9b2276ae0c790f262d190fac8813f03ef485a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-20 10:06:48 +0200

    Implement class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module comprises a class defined based on 5-base_geometry.py"""
 
+
 class BaseGeometry:
     """My BaseGeometry class"""
 
     def area(self):
         """
-        Compute the area of the geometry shape.
-
-        Raises:
-            NotImplementedError: If the area computation is not implemented for the shape.
+    area - Compute the area of the geometry shape.
+    Raises:
+        Exception: If the area computation is not implemented for the shape.
         """
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
