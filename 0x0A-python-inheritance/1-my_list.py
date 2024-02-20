f81097a32b9a208b075d1fea5fabe081a1ca78f7
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-20 08:52:58 +0200

    added test cases to test 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,5 +8,4 @@ class MyList(list):
     """MyList class"""
     def print_sorted(self):
         """Prints sorted list"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
