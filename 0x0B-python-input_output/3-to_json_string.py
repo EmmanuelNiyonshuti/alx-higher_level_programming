8aed807ade467b32cf5809e0b8e5ba9a8a0fa0cf
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-21 07:42:32 +0200

    sorting the dictionary keys alphabetically

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,7 @@ That returns a json string.
 """
 import json
 
+
 def to_json_string(my_obj):
     """
     to_json_string - gives a JSON string.
@@ -14,5 +15,5 @@ def to_json_string(my_obj):
     Return:
     JSON representation of an object (string).
     """
-    return json.dumps(my_obj)
 
+    return json.dumps(my_obj, sort_keys=True)
