142cd35738be161c6851f567a73a5509f2d3d8b6
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-21 09:56:19 +0200

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,13 +5,14 @@ This module provides functions for working with JSON data and text files.
 
 import json
 
+
 def load_from_json_file(filename):
     """
     load_from_json_file - creates an Object from a “JSON file.
     args:
     filename: The object file.
     Return:
-    Nothing.
+     The python object.
     """
     with open(filename, "r", encoding="utf-8") as my_file:
         return json.load(my_file)
