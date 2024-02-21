4757128d49acdb5e2e420eeb3b2e63b27c55dc8c
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-21 13:49:52 +0200

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,6 +3,7 @@
 comprises a student class.
 """
 
+
 class Student:
     """My student class"""
     def __init__(self, first_name, last_name, age):
@@ -10,5 +11,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
+        """ retrieves a dictionary representation of a Student instance"""
         return self.__dict__
