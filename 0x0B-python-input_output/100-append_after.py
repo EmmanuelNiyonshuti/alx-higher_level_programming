079fe3a11ee2e86c448c53db88319f9e0a9078ba
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-21 19:54:58 +0200

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 This module provides a function for working with files.
 """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """
     append_after -  inserts a line of text to a file,
