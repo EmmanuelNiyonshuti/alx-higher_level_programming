59c519ca601575a0a1801fb4297bdd6dfd479531
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-27 08:08:40 +0200

    return empty list when jso_string is None

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,7 +67,7 @@ class Base:
         Otherwise, return the list represented by json_string.
         """
         if json_string is None or len(json_string) == 0:
-            return '[]'
+            return []
         return json.loads(json_string)
 
     @classmethod
