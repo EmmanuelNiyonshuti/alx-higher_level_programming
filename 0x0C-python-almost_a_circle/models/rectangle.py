a084cf54be84870e8afe000dd3aafb2c772dc7c0
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-27 07:58:58 +0200

    used isinstance() checking for type

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,13 +15,14 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
-        if type(width) != int:
+
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if type(height) != int:
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if type(x) != int:
+        if not isinstance(x, int):
             raise TypeError("x must be an integer")
-        if type(y) != int:
+        if not isinstance(y, int):
             raise TypeError("y must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
@@ -41,7 +42,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setting the value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -55,7 +56,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setting the value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -69,7 +70,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setting the value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -83,7 +84,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setting the value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
