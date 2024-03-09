9777e98b89b34866d3453bd0ceb915eeb6bc366f
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-09 21:47:46 +0200

    adding attributes to object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Adds a new attribute to an object if possible.
+    Args:
+        obj: The object to which the attribute will be added.
+        attr_name: The name of the attribute.
+        attr_value: The value of the attribute.
+    Raises:
+        TypeError: If the object cannot have a new attribute.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr_name, attr_value)
