f0706a7fa2939840ca5f164862ec7f9cc925e3f5
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-26 20:31:17 +0200

    created a table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Creates a table in the current database
+-- table description: id int, name VARCHAR(256)
+-- data base name will be passed as an argument of the mysql command
+-- script will not fail if the table already exists
+CREATE table first_table(
+    id INT,
+    name VARCHAR(256)
+) IF NOT EXISTS;
