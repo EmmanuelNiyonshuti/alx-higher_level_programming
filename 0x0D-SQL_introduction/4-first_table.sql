72b271031e4659a59210515449d343913c049a51
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-26 20:50:55 +0200

    first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,7 +2,7 @@
 -- table description: id int, name VARCHAR(256)
 -- data base name will be passed as an argument of the mysql command
 -- script will not fail if the table already exists
-CREATE table first_table(
+CREATE table IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
-) IF NOT EXISTS;
+);
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table.
+-- Te database name will be passed as an argument of the mysql command
+DESC first_table;
