d324bf386d6cc716d69560395d02bbecd91b2483
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-27 13:13:42 +0200

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,4 +4,4 @@
 -- the number of records for that score with the label number
 -- the list will be sorted by the number of records (descending)
 -- the database name will be passed as an argument
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
