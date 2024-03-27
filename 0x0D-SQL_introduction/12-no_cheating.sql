69926aaffc1a59fe8c90f50cb00ff5e8822238f7
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-27 13:09:58 +0200

    Number By score

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- updates the score of Bob to 10 in the table second_table
 -- database name is passed as an argument of mysql command
-UPDATE score from second_table SET score = 10 WHERE name = Bob;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- lists the number of records with the same score in the table.
+-- the result should display
+-- the score
+-- the number of records for that score with the label number
+-- the list will be sorted by the number of records (descending)
+-- the database name will be passed as an argument
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
