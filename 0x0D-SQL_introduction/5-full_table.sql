5a23cdb5655207bd73fbc8568477e2fb747605ae
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-27 18:19:23 +0200

    show create

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Prints the full description of the table
 -- Te database name will be passed as an argument of the mysql command
 -- without using DESCRIBE or EXPLAIN statements.
-SHOW FULL COLUMNS FROM first_table;
+SHOW CREATE TABLE first_table;
