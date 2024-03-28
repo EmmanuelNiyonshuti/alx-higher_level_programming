dc8ba8ce33688af1318d26fdd6e22376520cf876
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-28 14:32:36 +0200

    USE hbtn_0d_usa db

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- the script will not fail if the database hbtn_0d_usa already exists
 -- the script will not fail if the table states already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
+-- USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
