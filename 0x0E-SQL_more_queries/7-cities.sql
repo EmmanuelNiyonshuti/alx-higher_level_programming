5cef004d33e254095da2c0ab1d3875a9c447dfa3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-28 12:39:27 +0200

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- This script create the database hbtn_0d_usa and the table cities in hbtn_0d_usa on mysql server
+-- the script will not fail if the database already exists
+-- the script will not fail if the table cities already exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS
+cities(id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id)
+);
