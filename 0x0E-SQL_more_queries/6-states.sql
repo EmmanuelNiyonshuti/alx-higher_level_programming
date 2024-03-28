9067628199cc86b7090f69238c714ed1c9c232f8
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-28 14:02:12 +0200

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,4 +3,4 @@
 -- the script will not fail if the table states already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
-(id INT UNIQUE AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL);
+(id INT AUTO_INCREMENT PRIMARY KEY(id), name VARCHAR(256) NOT NULL);
