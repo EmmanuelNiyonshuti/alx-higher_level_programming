cb0628c9a581eadd42025c0e6e3525f40bd366a1
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-22 16:13:41 +0200

    cli args

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const multiLges = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const size = multiLges.length;
+
+for (let i = 0; i < size; i++) {
+  console.log(multiLges[i]);
+}
