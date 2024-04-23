5e3e5cc7a46e1570de1aa3a9b6c846064186043f
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-23 15:53:59 +0200

    13. Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+  Define and export a function named 'add' that takes two parameters 'a' and 'b'.
+  This function returns the sum of the two parameters.
+*/
+exports.add = function (a, b) {
+  return (a + b);
+};
