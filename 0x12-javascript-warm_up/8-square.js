78c8ae191b8c041a20a2702b96a848af941cec86
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-23 09:03:22 +0200

    typo in task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const square = 'X';
 const arg = process.argv[2];
-if (isNaN(arg)) { console.log('Missing Size'); } for (let i = 0; i < arg; i++) { console.log(square.repeat(arg)); }
+if (isNaN(arg)) { console.log('Missing size'); } for (let i = 0; i < arg; i++) { console.log(square.repeat(arg)); }
