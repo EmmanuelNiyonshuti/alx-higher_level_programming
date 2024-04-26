fc1825fe7bdd52d2f0db7e7eea7a1ad3be4ed3c3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-26 14:32:13 +0200

    format instead of f string

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,9 @@ if __name__ == "__main__":
 
     state_name = sys.argv[4]
     """sql injection vulnerable"""
-    curr.execute(f"SELECT * FROM states WHERE name = '{state_name}' ORDER BY states.id ASC")
+    curr.execute("""
+                 SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC
+                 """.format(state_name))
     rows = curr.fetchall()
     for row in rows:
         print(row)
