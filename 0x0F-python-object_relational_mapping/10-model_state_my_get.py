9d47295d05f319aa7409bb84fb20c1fa8787131f
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-27 22:33:11 +0200

    checker error fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,26 +5,20 @@ from the database hbtn_0e_6_usa.
 """
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy import text
 import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
-
     engine = create_engine("mysql://{}:{}@localhost/{}".format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    querry = session.query(State).filter(State.name == text(':name'))
-    result = querry.params(name=sys.argv[4]).all()
+    querry = session.query(State).filter(State.name == sys.argv[4])
+    result = querry.first()
     if not result:
         print("Not Found")
     else:
-        [print(f"{state.id}") for state in result]
+        print(result.id)
 
-    #using bindparam class
-    # param_name = bindparam('param_name', value=sys.argv[4])
-
-    # querry = session.query(State).filter(State.name == param_name).order_by(State.id)
-    # result = querry.all()
+    session.close()
