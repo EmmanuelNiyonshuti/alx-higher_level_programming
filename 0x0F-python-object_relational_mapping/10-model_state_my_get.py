704088dd148fe381b59db07fd6d484f22ded7fec
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-27 22:46:00 +0200

    parameterized input for task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,20 +5,25 @@ from the database hbtn_0e_6_usa.
 """
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy import text
 import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
     engine = create_engine("mysql://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
+        user_name, passwd, db))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    querry = session.query(State).filter(State.name == sys.argv[4])
-    result = querry.first()
+    querry = session.query(State).filter(State.name == text(':name'))
+
+    result = querry.params(name=state_name).all()
     if not result:
         print("Not Found")
     else:
-        print(result.id)
-
-    session.close()
+        [print(f"{state.id}") for state in result]
