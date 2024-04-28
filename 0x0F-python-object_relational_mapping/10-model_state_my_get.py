25cf51f598663f4112bf97436558fc7888c66534
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-28 09:44:21 +0200

    parameterized querry

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,25 +4,25 @@ This script prints the state object with the name passed as argument
 from the database hbtn_0e_6_usa.
 """
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, bindparam
 import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     user_name = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
-    state_name = sys.argv[4]
 
     engine = create_engine("mysql://{}:{}@localhost/{}".format(
         user_name, passwd, db))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    querry = session.query(State).filter(State.name == state_name)
-
+    querry = session.query(State).filter(State.name == sys.argv[4])
     result = querry.first()
     if not result:
         print("Not Found")
     else:
         print(result.id)
+    session.close()
