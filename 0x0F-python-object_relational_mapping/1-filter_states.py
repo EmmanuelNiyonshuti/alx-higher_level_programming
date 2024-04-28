c2f779590de7c7d904c9c1aac069ad6faec31061
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-28 13:04:04 +0200

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,16 +14,18 @@ if __name__ == "__main__":
     passwd = sys.argv[2]
     db = sys.argv[3]
     """connect to the db"""
-    connection = MySQLdb.connect(host = 'localhost', port = 3306, user = username, passwd = passwd, db = db)
+    conn = MySQLdb.connect(host = 'localhost', port = 3306, user = username, passwd = passwd, db = db)
     """create a cursor and use it to execute sql queries"""
-    cursor = connection.cursor()
-    cursor.execute("""SELECT * FROM states
-                WHERE name LIKE 'N%' ORDER BY states.id ASC""")
+    cur = conn.cursor()
+    cur.execute("""
+                SELECT * FROM states WHERE SUBSTRING(name, 1, 1) = 'N'
+                ORDER BY states.id ASC
+                """)
 
     """fetch the rows and print the results"""
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     """close the connection"""
-    cursor.close()
-    connection.close()
+    cur.close()
+    conn.close()
