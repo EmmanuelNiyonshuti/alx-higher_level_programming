a2ad6c234ee83464090c7ec7eacd7233c7ef4059
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-12 18:23:12 +0200

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 makes a request to https://alx-intranet.hbtn.io/status.
+must use the package requests.
 """
 import requests
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
 
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type:", type(str(r)))
     print("\t- content:", r.content.decode('utf-8'))
-    
