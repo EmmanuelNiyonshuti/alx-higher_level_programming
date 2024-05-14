af566ef5d2b6a225f6b2d5ff8555c899526b45ba
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-14 15:24:31 +0200

    100-status_code.sh

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,17 +5,18 @@ comprise a find_peak algorithm
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
+    """
+    Finds a peak in a list of unsorted integers.
+    peak is a number which is greater than or equal to it's neighbours.
+    this function starts from 0 of the list,
+    compare each current number to it's neighbours
+    and then returns peak whenever it encounters it.
+    """
     n = len(list_of_integers)
-    for i in range(n):
+    peak = None
+    for i in range(1, n - 1):
         left = i - 1
         right = i + 1
-        if left > - 1 and right < n:
-            if list_of_integers[i] >= list_of_integers[left] and list_of_integers[i] >= list_of_integers[right]:
-                return list_of_integers[i]
-        elif left == -1:
-            if list_of_integers[i] >= list_of_integers[right]:
-                return list_of_integers[i]
-        elif right == n:
-            if list_of_integers[i] >= list_of_integers[left]:
-                return list_of_integers[i]
+        if (list_of_integers[i] >= list_of_integers[left] and list_of_integers[i] >= list_of_integers[right]):
+            peak = list_of_integers[i]
+    return peak
