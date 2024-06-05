58842f809e316e5507d77a0a9d44e23def9ecc45
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-05 12:42:27 +0200

    fix a typo in task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // -prints the number of movies where the character “Wedge Antilles” is present.
+// - use requests module
 
 const request = require('request');
 const url = process.argv[2];
-
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
@@ -11,9 +11,11 @@ request(url, (err, response, body) => {
   }
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
+    const peopleEndpoint = url.replace('films', 'people');
+    const characterId = 18;
     let countMovie = 0;
     data.results.forEach(movie => {
-      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      if (movie.characters.includes(`${peopleEndpoint}/${characterId}/`)) {
         countMovie++;
       }
     });
